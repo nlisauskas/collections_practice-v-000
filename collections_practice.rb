0ef6ef1dd01c86238a9f1cac60f10fb97a015801
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-11 10:57:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  new_array = []
+  i = 3
+  new_array << array[0]
+  new_array[1] = array[2]
+  new_array[2] = array[1]
+  while i < array.length
+    new_array << array[i]
+    i += 1
+  end
+  new_array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |name|
+    name[2] = "$"
+    new_array << name
+  end
+  new_array
+end
+
+def find_a(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    if array[i][0] == "a" then new_array << array[i] end
+      i += 1
+    end
+    new_array
+end
+
+def sum_array(array)
+  sum = 0
+  i = 0
+  while i < array.length
+    sum += array[i].to_i
+    i += 1
+  end
+  sum
+end
+
+def add_s(array)
+array.each_with_index.collect do |element, index|
+  array[index] = "#{element}s"
+end
+end
